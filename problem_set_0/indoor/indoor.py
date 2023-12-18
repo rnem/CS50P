1366cda4f7bfa59ba0016c35e8adfde83ac880de
Roger Nem <{ID}+{username}@users.noreply.github.com>
2023-12-18 13:59:30 -0800

    adding problem set 0 - indoor

---
+++ problem_set_0/indoor/indoor.py
@@ -0,0 +1,11 @@
+"""
+CS50 - Indoor Voice
+Author: Roger Nem
+About: Prompts the user for input and then outputs that same input in lowercase
+"""
+
+# Prompts the user for input
+usr_input = input("Please say something: ")
+
+# https://docs.python.org/3/library/stdtypes.html - str.lower()
+print(usr_input.lower())
